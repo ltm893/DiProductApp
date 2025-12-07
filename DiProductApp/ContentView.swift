52ab490a64678e2e1311c97ae0376e2540718d66
Louis Melchiorre <ltm893@icloud.com>
2025-12-07 08:44:21 -0500

    Initial Commit

---
+++ DiProductApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiProductApp
+//
+//  Created by Louis Melchiorre        on 12/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiProductApp/DiProductAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiProductAppApp.swift
+//  DiProductApp
+//
+//  Created by Louis Melchiorre        on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DiProductAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
